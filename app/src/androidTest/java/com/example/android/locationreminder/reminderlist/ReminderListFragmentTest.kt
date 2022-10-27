bb200be32b897f2ac8dd8d3b2c747efcbdaf1868
Zeyad Maamoun <zeyadmamoun952@gmail.com>
2022-10-27 17:51:05 +0200

    4th project for udacity advanced android nanodegree

--- app/src/androidTest/java/com/example/android/locationreminder/reminderlist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/locationreminder/reminderlist/ReminderListFragmentTest.kt
@@ -8,27 +8,26 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.example.android.locationreminder.locationreminders.reminderslist.ReminderListFragment
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Test
-import org.junit.runner.RunWith
 import com.example.android.locationreminder.R
 import com.example.android.locationreminder.data.FakeAndroidDataSource
 import com.example.android.locationreminder.locationreminders.data.ReminderDataSource
 import com.example.android.locationreminder.locationreminders.reminderslist.ReminderDataItem
+import com.example.android.locationreminder.locationreminders.reminderslist.ReminderListFragment
 import com.example.android.locationreminder.locationreminders.reminderslist.ReminderListFragmentDirections
 import com.example.android.locationreminder.locationreminders.reminderslist.RemindersListViewModel
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
-import javax.sql.DataSource
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
