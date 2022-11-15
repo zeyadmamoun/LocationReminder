224e535f394c9660ac817af0bde7a3911d0f8fec
Zeyad Maamoun <zeyadmamoun952@gmail.com>
2022-11-15 21:58:45 +0200

    4th project for udacity advanced android nanodegree

--- app/src/androidTest/java/com/example/android/locationreminder/savereminder/SaveReminderFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/locationreminder/savereminder/SaveReminderFragmentTest.kt
@@ -5,6 +5,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -16,6 +17,8 @@ import com.example.android.locationreminder.locationreminders.data.ReminderDataS
 import com.example.android.locationreminder.locationreminders.reminderslist.ReminderDataItem
 import com.example.android.locationreminder.locationreminders.savereminder.SaveReminderFragment
 import com.example.android.locationreminder.locationreminders.savereminder.SaveReminderViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -60,6 +63,25 @@ class SaveReminderFragmentTest {
         onView(withId(R.id.reminderTitle)).perform(replaceText(reminder.title))
         onView(withId(R.id.reminderDescription)).perform(replaceText(reminder.description))
         saveReminderViewModel.validateAndSaveReminder(reminder)
+
+        assertEquals("Reminder Saved !", saveReminderViewModel.showToast.getOrAwaitValue())
+    }
+
+    // the same test but there is error happening when i click on the saveReminder button.
+    @Test
+    fun testingToastWithButton_whenNewReminderSaved() {
+        val reminder = ReminderDataItem("Testing Reminder","Testing Desc","location",20.0,40.0,UUID.randomUUID().toString())
+        val scenario = launchFragmentInContainer<SaveReminderFragment>(Bundle(),R.style.Theme_LocationReminder)
+        saveReminderViewModel.longitude.value = 20.0
+        saveReminderViewModel.latitude.value = 40.0
+        saveReminderViewModel.selectedPOI.value = PointOfInterest(LatLng(40.0,20.0),"2","Location")
+        saveReminderViewModel.reminderTitle.value = "Testing Reminder"
+        saveReminderViewModel.reminderDescription.value = "Testing Desc"
+        saveReminderViewModel.reminderSelectedLocationStr.value = "Location"
+
+        onView(withId(R.id.reminderTitle)).perform(replaceText(reminder.title))
+        onView(withId(R.id.reminderDescription)).perform(replaceText(reminder.description))
+        onView(withId(R.id.saveReminder)).perform(click())
 
         assertEquals("Reminder Saved !", saveReminderViewModel.showToast.getOrAwaitValue())
     }
